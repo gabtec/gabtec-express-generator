161685ee66113f2e0759b4d83edbd6af5934948f
Gabriel Martins <gabtec@sapo.pt>
2022-09-28 11:53:58 +0100

    bug fix: banner import statement

--- tests/snapshots/banner-import.snap.js
+++ tests/snapshots/banner-import.snap.js
@@ -1,6 +1,6 @@
 const importData = [
 	'require("colors");',
-	'const VERSION = require("./package.json").version;',
+	'const VERSION = require("../package.json").version;',
 	'const banner = require("./src/assets/banner.js");',
 	'banner.print("api v" + VERSION);',
 ];
